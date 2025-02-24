b0853b5326e44e1db44f59fdd9553469479fd262
agustinmontes10 <montesagustin99@gmail.com>
2025-02-24 11:43:27 -0300

    fix env

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import { NextResponse } from 'next/server';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Faltan las variables de entorno de Supabase');
